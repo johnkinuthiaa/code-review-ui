60a01ac5e0460a02b9f61a34a04569cf861ae158
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-09 16:30:13 +0300

    response from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,44 @@
 "use client"
 import CodeIcon from '@mui/icons-material/Code';
 import Analysis from "@/components/ui/analysis";
+import {useState} from "react";
 
 
 export default function Home() {
+    const[codeForAnalysis,setCodeForAnalysis] =useState<string>("");
+    const[analyseCode,setAnalyseCode] =useState(false)
+    const[message,setMessage]=useState<string>("")
   return (
       <div className={"w-[90%] m-[0 auto] flex flex-col items-center justify-center text-black bg-white"}>
         <h1 className={"font-extrabold text-4xl mt-10 mb-2 "}>Code Analysis system</h1>
-        <p className={" text-xl m-2"}>Submit your code to analyze for bugs,security vulnerabilities,performance issues and best coding practices</p>
+        <p className={" m-4"}>Submit your code to analyze for bugs,security vulnerabilities,performance issues and best coding practices</p>
         <div className={"rounded-2xl border border-gray-300 flex flex-col w-8/12 p-5 "}>
           <div className={"flex flex-col leading-7"}>
             <h4 className={"font-bold text-2xl"}>Code Input</h4>
             <p className={"mt-3 mb-6"}>Paste your code Snippet below</p>
           </div>
-          <textarea className={"outline active:outline-black font-mono rounded-3xl h-98 overflow-scroll w-[95%] scroll-smooth resize-none m-6 p-5 border border-gray-300"}
-                    placeholder={"// paste your code here..."} />
+            {message&&<p className={"text-red-500 animate-bounce p-1 "}>{message}</p>}
+          <textarea spellCheck={false} className={"outline active:outline-black font-mono rounded-3xl h-98 overflow-scroll w-[95%] scroll-smooth resize-none m-6 p-5 border border-gray-300"}
+                    placeholder={"// paste your code here..."} onChange={(e)=>{
+                        setCodeForAnalysis(e.target.value)
+          }}/>
           <button
-              className={"p-3 text-blue-700 bg-black w-fit cursor-pointer font-bold rounded-2xl flex items-center"}><CodeIcon /> Analyze Code</button>
+              className={` ${analyseCode&&"cursor-not-allowed"} p-3 text-white bg-black w-fit cursor-pointer font-bold rounded-2xl flex items-center`}
+              onClick={(e)=>{
+                  e.preventDefault()
+                  if(codeForAnalysis.length>10) {
+                      setAnalyseCode(true)
+                  }else {
+                      setMessage("please input code")
+                      setTimeout(()=>{setMessage("")},3000)
+                  }
+              }}
+          >
+              <CodeIcon />
+              Analyze Code
+          </button>
         </div>
-          <Analysis/>
+          {analyseCode &&<Analysis code={codeForAnalysis}/>}
       </div>
   )
 }
--- src/components/ui/analysis.tsx
+++ src/components/ui/analysis.tsx
@@ -1,9 +1,55 @@
-export default function Analysis(code){
+import React, {useEffect, useState} from "react";
 
-    return(
-        <div className={"flex flex-col mt-4 leading-7"}>
+interface AnalysisProps {
+    code?: string
+}
+
+
+
+const Analysis = ({code}: AnalysisProps) => {
+    const[message,setMessage]=useState<string>("")
+    const[analysisResponse,setAnalysisResponse] =useState<string>()
+    useEffect(() => {
+        fetchData()
+    }, []);
+    const fetchData =async ()=>{
+        const myHeaders =new Headers()
+        myHeaders.append("Content-Type","application/json")
+        try{
+            const response=await fetch(`http://localhost:8080/api/v1/ai/analyse`,{
+                method:"POST",
+                headers:myHeaders,
+                body:JSON.stringify({
+                    "message": code
+                })
+            })
+            if(response.ok){
+                const data =await response.json()
+                if(data.statusCode !=200){
+                    setMessage(data.message)
+                }else{
+                    setAnalysisResponse(data.response)
+                }
+
+            }else{
+                setMessage("server response Error")
+            }
+
+        }catch (error) {
+            setMessage(""+error)
+        }
+    }
+    return (
+        <div className={"flex flex-col mt-4 leading-7 items-center w-8/12 leading-8"}>
             <h2 className={"font-bold text-2xl text-black"}>Codey&#39;s analysis Details</h2>
-            <p>{code}</p>
+            <p>This is the result of the analysis</p>
+            {message&&<p className={"font-bold text-red-500 "}>{message}</p>}
+            <div className={"p-4 flex flex-col"}>
+                <p>{analysisResponse?.replace(/\\n/g, '\n')
+                    .replace(/\\u003c/g, '<')
+                    .replace(/\\u003e/g, '>')}</p>
+            </div>
         </div>
     )
 }
+export default Analysis
